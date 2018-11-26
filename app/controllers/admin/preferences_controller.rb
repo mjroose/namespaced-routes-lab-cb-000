cedb02312c159cbb8abeb1bebdebee9b0aa782a2
Michael Roose <mjroose@gmail.com>
2018-11-26 20:26:32 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -2,4 +2,8 @@ class Admin::PreferencesController < ApplicationController
   def show
     @preferences = Preference.find_or_create_by(id: 1)
   end
+
+  def update
+    raise params.inspect
+  end
 end
