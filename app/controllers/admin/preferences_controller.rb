d6fee42ccafa83df7bebdba8f764463c31f7a887
Michael Roose <mjroose@gmail.com>
2018-11-26 20:28:04 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -6,4 +6,9 @@ class Admin::PreferencesController < ApplicationController
   def update
     raise params.inspect
   end
+
+  private 
+    def preference_params
+      params.require(:preference).permit(:artist_sort_order, :song_sort_order, :allow_create_artists, :allow_create_songs)
+    end
 end
